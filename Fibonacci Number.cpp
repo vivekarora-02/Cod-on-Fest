5844a86b11298cbb2f70d8bd3e80f4fd94d2deab
VIVEK  ARORA <73037125+vivekarora-02@users.noreply.github.com>
2021-09-03 11:48:04 +0530

    Leetcode Question

---
+++ Fibonacci Number.cpp
@@ -0,0 +1,19 @@
+// Question URL - https://leetcode.com/problems/fibonacci-number/
+
+
+class Solution {
+public:
+    int fib(int n) {
+      // using DP
+      int dp[n+2];
+      dp[0] = 0;
+      dp[1] = 1;
+      for(int i=2;i<=n;++i){
+         dp[i] = dp[i-1] + dp[i-2];
+      }
+        
+     return dp[n];
+        
+        
+    }
+};
